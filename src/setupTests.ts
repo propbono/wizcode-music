1cf97c74cb9670f70f1f1ac7cae5b7cbf500bd91
G M <propbono@gmail.com>
2023-06-22 07:18:49 -0400

    chore: setup vitest to work with msw

---
+++ src/setupTests.ts
@@ -0,0 +1,6 @@
+import { server } from "./mocks/server";
+import { afterAll, afterEach, beforeAll } from "vitest";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
