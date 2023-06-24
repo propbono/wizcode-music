5c29f720c6d9534bebd922e105fb12996e7ce9d4
G M <propbono@gmail.com>
2023-06-24 18:49:12 -0400

    feat: update album-list test file to include query client and query cache and invalidate query cache after each test

--- src/components/album-list/album-list.test.tsx
+++ src/components/album-list/album-list.test.tsx
@@ -2,29 +2,50 @@ import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
 import { AlbumList } from "./album-list";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryCache, QueryClient, QueryClientProvider } from "react-query";
+import { ITUNES_URL } from "../../services/itunes/itunes-service";
+import { server } from "../../mocks/server";
+import { rest } from "msw";
 
-const queryClient = new QueryClient();
+const queryCache = new QueryCache();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
-beforeEach(() => {
-  render(
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <AlbumList />
-      </RecoilRoot>
-    </QueryClientProvider>
-  );
+afterEach(() => {
+  queryCache.clear();
 });
 
 describe("album list component tests", async () => {
-  it("Should have a title", () => {
+  it("should have a title", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <AlbumList />
+        </RecoilRoot>
+      </QueryClientProvider>
+    );
+
     const h2 = screen.getByText("Top Albums");
 
     expect(h2).toBeInTheDocument();
   });
 
   it("should render an album list", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <AlbumList />
+        </RecoilRoot>
+      </QueryClientProvider>
+    );
+
     const album1 = await screen.findByText("chemistry");
+
     const album2 = await screen.findByText(
       "Mystical Magical Rhythmical Radical Ride"
     );
@@ -34,6 +55,20 @@ describe("album list component tests", async () => {
   });
 
   it("should display error message when fetch throws error", async () => {
+    server.use(
+      rest.get(ITUNES_URL, async (_, res, ctx) => {
+        return res(ctx.status(500), ctx.json({ message: "Error" }));
+      })
+    );
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <AlbumList />
+        </RecoilRoot>
+      </QueryClientProvider>
+    );
+
     const errorMessage = await screen.findByText(
       "Failed to fetch albums. Please try again later."
     );
